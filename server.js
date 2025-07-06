f5d8714644f21c77d5272de452a212e8bcfd588f
Amal Sindhu Sasidharan <amal0109@gmail.com>
2025-07-06 17:05:27 +0530

    using modemon for automatic server reload; fetch userlist api

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ const port = 3000;
 const app = express();
 
 
+let userList = [
+    {
+        userName : 'Ram',
+        email: 'ram@testmail.com'
+    }
+]
+
 app.get('/',(req,res)=>{
     res.send('Hello World')
-})
+});
+
+app.get('/about',(req,res)=>{
+    res.send('About url hit');
+});
+
+app.get('/userList',(req,res)=>{
+    res.status(200).json(userList);
+});
 
 app.listen(port,()=>{
     console.log(`Express app server is listening on port - ${port}`);
